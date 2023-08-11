916336d1d7fe8179b3fdea5d0375f7659fad6657
Ahmed Rebai <ahmed.rebai2@gmail.com>
2023-08-11 21:32:39 +0200

    Create unet.py
    To simplify the use of the code, I created a py file.

---
+++ unet.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+
+# Define a double convolution block
+def double_conv(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=3),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_channels, out_channels, kernel_size=3),
+        nn.ReLU(inplace=True)
+    )
+
+# Crop the tensor to match target tensor's size
+def crop_img(tensor, target_tensor):
+    target_size = target_tensor.size()[2]
+    tensor_size = tensor.size()[2]
+    delta = tensor_size - target_size
+    delta = delta // 2
+    return tensor[:, :, delta:tensor_size - delta, delta:tensor_size - delta]
+
+# Define the U-Net model
+class UNet(nn.Module):
+    def __init__(self):
+        super(UNet, self).__init__()
+        self.max_pool_2x2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        # Encoder layers
+        self.down_conv_1 = double_conv(1, 64)
+        self.down_conv_2 = double_conv(64, 128)
+        self.down_conv_3 = double_conv(128, 256)
+        self.down_conv_4 = double_conv(256, 512)
+        self.down_conv_5 = double_conv(512, 1024)
+        
+        # Decoder layers
+        self.up_trans_1 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=2, stride=2)
+        self.up_conv_1 = double_conv(1024, 512)
+        
+        self.up_trans_2 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=2, stride=2)
+        self.up_conv_2 = double_conv(512, 256)
+        
+        self.up_trans_3 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=2, stride=2)
+        self.up_conv_3 = double_conv(256, 128)
+        
+        self.up_trans_4 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
+        self.up_conv_4 = double_conv(128, 64)
+        
+        # Output layer
+        self.out = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1)
+
+    def forward(self, image):
+        # Encoder
+        x1 = self.down_conv_1(image)
+        x2 = self.max_pool_2x2(x1)
+        x3 = self.down_conv_2(x2)
+        x4 = self.max_pool_2x2(x3)
+        x5 = self.down_conv_3(x4)
+        x6 = self.max_pool_2x2(x5)
+        x7 = self.down_conv_4(x6)
+        x8 = self.max_pool_2x2(x7)
+        x9 = self.down_conv_5(x8)
+        
+        # Decoder
+        x = self.up_trans_1(x9)
+        y = crop_img(x7, x)
+        x = self.up_conv_1(torch.cat([x, y], 1))
+        
+        x = self.up_trans_2(x)
+        y = crop_img(x5, x)
+        x = self.up_conv_2(torch.cat([x, y], 1))
+        
+        x = self.up_trans_3(x)
+        y = crop_img(x3, x)
+        x = self.up_conv_3(torch.cat([x, y], 1))
+        
+        x = self.up_trans_4(x)
+        y = crop_img(x1, x)
+        x = self.up_conv_4(torch.cat([x, y], 1))
+        
+        # Output
+        x = self.out(x)
+        return x
+
+if __name__ == "__main__":
+    # Create a sample input image
+    image = torch.rand((1, 1, 572, 572))
+    
+    # Instantiate the U-Net model
+    model = UNet()
+    
+    # Run the input image through the model
+    segmentation_map = model(image)
+    
+    print("Segmentation Map Shape:", segmentation_map.shape)
